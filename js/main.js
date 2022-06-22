a85d8e8f065023c131a872a1520e144d60051a52
Mehaned <mehaned.ahmed7@yahoo.com>
2022-06-22 14:33:57 +0200

    first commit

--- js/main.js
+++ js/main.js
@@ -1,40 +1,5 @@
-let progressSpans = document.querySelectorAll(".the-progress span");
-let section = document.querySelector(".our-skills");
 
-let nums = document.querySelectorAll(".stats .number");
-let statsSection = document.querySelector(".stats");
-let started = false; // Function Started ? No
-
-window.onscroll = function () {
-  // Skills Animate Width
-  if (window.scrollY >= section.offsetTop - 250) {
-    progressSpans.forEach((span) => {
-      span.style.width = span.dataset.width;
-    });
-  }
-  // Stats Increase Number
-  if (window.scrollY >= statsSection.offsetTop) {
-    if (!started) {
-      nums.forEach((num) => startCount(num));
-    }
-    started = true;
-  }
-};
-
-
-function startCount(el) {
-  let goal = el.dataset.goal;
-  let count = setInterval(() => {
-    el.textContent++;
-    if (el.textContent == goal) {
-      clearInterval(count);
-    }
-  }, 2000 / goal);
-}
-
-
-
-let countDownDate = new Date("Dec 31, 2021 23:59:59").getTime();
+let countDownDate = new Date("Dec 31, 2022 23:59:59").getTime();
 // console.log(countDownDate);
 
 let counter = setInterval(() => {
@@ -60,4 +25,38 @@ let counter = setInterval(() => {
     clearInterval(counter);
   }
 }, 1000);
-  
+
+
+
+let progressSpans = document.querySelectorAll(".the-progress span");
+let section = document.querySelector(".our-skills");
+
+let nums = document.querySelectorAll(".stats .number");
+let statsSection = document.querySelector(".stats");
+let started = false; // Function Started ? No
+
+window.onscroll = function () {
+  // Skills Animate Width
+  if (window.scrollY >= section.offsetTop - 250) {
+    progressSpans.forEach((span) => {
+      span.style.width = span.dataset.width;
+    });
+  }
+  // Stats Increase Number
+  if (window.scrollY >= statsSection.offsetTop) {
+    if (!started) {
+      nums.forEach((num) => startCount(num));
+    }
+    started = true;
+  }
+};
+
+function startCount(el) {
+  let goal = el.dataset.goal;
+  let count = setInterval(() => {
+    el.textContent++;
+    if (el.textContent == goal) {
+      clearInterval(count);
+    }
+  }, 2000 / goal);
+}
